3cac9029cbd292738f8da94c0980dcbafd641264
mark mialik <markmialik@gmail.com>
2018-11-08 14:40:29 -0800

    tester

--- src/finalproject/FinalProject.java
+++ src/finalproject/FinalProject.java
@@ -11,41 +11,25 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
  *
  * @author JuliusGarma
+ * @authur MarkMialik
  */
 public class FinalProject extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+  
     }
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        new Game().setVisible(true);
     }
     
 }
